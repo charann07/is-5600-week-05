4f3c688b1b41ae447297846ca8c9b4dddd07c9e0
charann07 <92818954+charann07@users.noreply.github.com>
2025-09-22 02:59:34 +0000

    Lab 5 submission

--- api.js
+++ api.js
@@ -1,16 +1,11 @@
 const path = require('path')
 const Products = require('./products')
 const autoCatch = require('./lib/auto-catch')
-
-/**
- * Handle the root route
- * @param {object} req
- * @param {object} res
-*/
+const Orders = require('./orders')
+ 
 function handleRoot(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 }
-
 /**
  * List all products
  * @param {object} req
@@ -26,8 +21,6 @@ async function listProducts(req, res) {
     tag
   }))
 }
-
-
 /**
  * Get a single product
  * @param {object} req
@@ -35,45 +28,87 @@ async function listProducts(req, res) {
  */
 async function getProduct(req, res, next) {
   const { id } = req.params
-
   const product = await Products.get(id)
   if (!product) {
     return next()
   }
-
   return res.json(product)
 }
 
 /**
- * Create a product
- * @param {object} req 
- * @param {object} res 
+ * Create a new product
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct(req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
- * Edit a product
- * @param {object} req
- * @param {object} res
- * @param {function} next
+ * Update a product
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
 async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
 }
 
 /**
  * Delete a product
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
 async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
 }
+
+/**
+ * Create an order
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function createOrder(req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(order)
+}
+
+/**
+ * List orders
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function listOrders(req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({
+    offset: Number(offset),
+    limit: Number(limit),
+    productId,
+    status
+  })
+
+  res.json(orders)
+}
+
+async function editOrder(req, res, next) {
+  const order = await Orders.edit(req.params.id, req.body)
+  res.json(order)
+}
+
+async function deleteOrder(req, res, next) {
+  const response = await Orders.destroy(req.params.id)
+  res.json(response)
+}
+
 
 module.exports = autoCatch({
   handleRoot,
@@ -81,5 +116,9 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  createOrder,
+  listOrders,
+  editOrder,
+  deleteOrder
 });
